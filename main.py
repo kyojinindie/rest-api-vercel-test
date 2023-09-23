adf3afb716c3ebe6806cdef0a01dc23b750aad5e
kyojin-indie <kyojinindie@gmail.com>
2023-09-22 22:36:44 -0600

    refactor

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+import os
+from langchain.llms import OpenAI
+from pydantic import BaseModel
+
+
+class Request(BaseModel):
+    prompt: str
+
 
 app = FastAPI()
 
+"""
+A health check endpoint.
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World siuuuuuuuu"}
+Returns:
+    dict: The status of the health check.
+"""
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
 
+"""
+An asynchronous function that handles POST requests to the "/api/v1/text-davinci-003" endpoint.
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+Args:
+    request (Request): The incoming request object.
+    OPENAI_API_KEY (str): The API key for OpenAI.
+
+Returns:
+    dict: A dictionary containing the response message.
+"""
+@app.post("/api/v1/text-davinci-003")
+async def text_davinci_003(request: Request, OPENAI_API_KEY: str = Header(...)):
+    prompt = request.prompt
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+    llm = OpenAI(model_name="text-davinci-003")
+    response = llm(prompt)
+    return {"message": response}
